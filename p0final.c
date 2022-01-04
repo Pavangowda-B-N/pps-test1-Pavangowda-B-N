f98fb299a970f73bb246ef43229ae8c6a4c5be63
Pavan Gowda <pavangowda80731@gmail.com>
2022-01-04 15:45:07 +0530

    p0final.c is completed

--- p0final.c
+++ p0final.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-  printf(" My Name is Pavan \n My Friends Name is:\n Pratik \n ChandraPrakash \n Sagar \n Suhas \n Harish\n\t\t\t*************\n");
+  printf(" My Name is Pavan \n");
   return 0;
 }
